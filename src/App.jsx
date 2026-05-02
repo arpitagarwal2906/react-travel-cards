7a88f08a90524f051654a5a332e423b527762791
Arpit Agarwal <arpag2906@gmail.com>
2026-05-02 09:43:22 +0530

    Add multiple travel journal entries with dynamic props and update CSS styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,40 @@ function App() {
   return (
     <>
       <Header />
-      <Entry />
+      <Entry
+        img={{
+          src: "https://scrimba.com/links/travel-journal-japan-image-url",
+          alt: "mount fuji",
+        }}
+        title="Mount Fuji"
+        country="JAPAN"
+        googleMapsUrl="https://www.google.com/maps/place/Mount+Fuji/@35.3606255,138.7273634,15z/data=!3m1!4b1!4m5!3m4!1s0x6019629a42fdc899:0xa6a1fcc916f8e7e!8m2!3d35.3606255!4d138.7273634"
+        date="12 Jan, 2021 - 24 Jan, 2021"
+        text="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,389 feet). It is an active stratovolcano that last erupted in 1707-1708. Mount Fuji is a popular destination for tourists and climbers, offering stunning views and a unique cultural experience."
+      />
+      <Entry
+        img={{
+          src: "https://upload.wikimedia.org/wikipedia/commons/4/40/Sydney_Opera_House_Sails.jpg",
+          alt: "Sydney Opera House",
+        }}
+        title="Sydney Opera House"
+        country="AUSTRALIA"
+        googleMapsUrl="https://www.google.com/maps/place/Sydney+Opera+House/"
+        date="27 May, 2021 - 8 Jun, 2021"
+        text="The Sydney Opera House is a multi-venue performing arts centre and one of the 20th century's most famous and distinctive buildings."
+      />
+
+      <Entry
+        img={{
+          src: "https://upload.wikimedia.org/wikipedia/commons/a/a8/Tour_Eiffel_Wikimedia_Commons.jpg",
+          alt: "Eiffel Tower",
+        }}
+        title="Eiffel Tower"
+        country="FRANCE"
+        googleMapsUrl="https://www.google.com/maps/place/Eiffel+Tower/"
+        date="10 Aug, 2021 - 18 Aug, 2021"
+        text="The Eiffel Tower is a wrought-iron lattice tower on the Champ de Mars in Paris, France. It is one of the most recognizable structures in the world."
+      />
     </>
   );
 }
--- src/Entry.jsx
+++ src/Entry.jsx
@@ -1,30 +1,18 @@
-export default function Entry() {
+export default function Entry(props) {
   return (
     <article className="journal-entry">
       <div className="main-img-container">
-        <img
-          className="main-img"
-          src="https://scrimba.com/links/travel-journal-japan-image-url"
-          alt="mount fuji"
-        />
+        <img className="main-img" src={props.img.src} alt={props.img.alt} />
       </div>
       <div>
         <img className="marker" src="..\marker.png" alt="location marker" />
-        <span>JAPAN</span>
-        <a
-          href="https://www.google.com/maps/place/Mount+Fuji/@35.3606255,138.7273634,15z/data=!3m1!4b1!4m5!3m4!1s0x6019629a42fdc899:0xa6a1fcc916f8e7e!8m2!3d35.3606255!4d138.7273634"
-          target="_blank"
-        >
+        <span>{props.country}</span>
+        <a href={props.googleMapsUrl} target="_blank">
           View on Google Maps
         </a>
-        <h2>Mount Fuji</h2>
-        <p>12 Jan, 2021 - 24 Jan, 2021</p>
-        <p className="entry-description">
-          Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters
-          (12,389 feet). It is an active stratovolcano that last erupted in
-          1707-1708. Mount Fuji is a popular destination for tourists and
-          climbers, offering stunning views and a unique cultural experience.
-        </p>
+        <h2>{props.title}</h2>
+        <p>{props.date}</p>
+        <p className="entry-description">{props.text}</p>
       </div>
     </article>
   );
